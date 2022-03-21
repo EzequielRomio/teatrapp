fdac8314ce6b9284d391d496ad5bfade28abf0fe
Ezequiel Romio <romio.ezequiel@gmail.com>
2022-03-20 21:46:34 -0300

    testing Helmet

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import { getTheaters, getDay, getBillboard } from './actions';
 import Plays from './components/Plays';
 import Title from './components/Title';
 import './App.css';
+import { Helmet } from 'react-helmet'
 
 function App() {
   const [theaters, setTheaters] = useState([])
   const [currentDay, setCurrentDay] = useState(0)
   const [billboard, setBillboard] = useState([])
-  const setDescription = desc => {
-    const el = document.querySelector("meta[name='description']");
-    el.setAttribute('content',desc)
-  }
+
   useEffect(() => {
     setTheaters(getTheaters)
     setCurrentDay(getDay)
     setBillboard(getBillboard)
-    const el = document.querySelector("meta[name='description']");
-    el.setAttribute('content', 'Oferta teatral del día de hoy en la Ciudad de Buenos Aires')
   }, [])
 
   return (
     <div className="App">
+      <Helmet>
+        <title>Catelera del día</title>
+        <meta
+          name="description"
+          content=" jdkladjslajdlOferta teatral del día de hoy en la Ciudad de Buenos Aires"
+        />
+      </Helmet>
       <Title />
       <hr style={{height: '26px', backgroundColor: '#BC8F8F'}}></hr>
       {theaters && theaters.length && billboard && billboard.length && <Plays theaters={theaters} billboard={billboard} currentDay={currentDay} />}
