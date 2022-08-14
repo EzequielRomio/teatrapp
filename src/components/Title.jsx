5b91828736219c9689b343cd0acf636ac49329d4
EzequielRomio <romio.ezequiel@gmail.com>
2022-08-14 11:25:29 -0300

    style updated

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,29 +1,33 @@
-import masks from '../assets/masks.png'
+import masks from '../assets/black-masks.png'
 import theaterBg from '../assets/theatre-bg.png'
 
 const style = {
-  backgroundColor: '#282c34',
-  backgroundImage: `url(${theaterBg})`,
-  backgroundSize: 'cover',
-  minHeight: '100vh',
+  // backgroundColor: '#282c34',
+  // backgroundImage: `url(${theaterBg})`,
+  // backgroundSize: 'cover',
+  minHeight: '20vh',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
   fontSize: 'calc(48px + 2vmin)',
   color: 'white',
-  textShadow: '6px 6px 10px #000'
+  textShadow: '6px 6px 10px #000',
 }
 
 const Title = () => {
   return (
-    <header style={style} className={'title-font'}>
-      <div style={{height: '160px'}}></div>
-      <span style={{transform: 'rotate(-3deg)'}}>A LLENAR LAS SALAS!</span>
-      
-      <img src={masks} width='180' alt='happy and sad theater masks'></img>      
-      
+    <header style={style} className={'bg-grad'}>
+      <div>
+      <span style={{
+        //transform: 'rotate(-8deg)', 
+        wordBreak: 'break-word', margin: '40px 20px 40px 20px'}} className={'title-font'}>Dame Teatro</span>
+      {/* <img src={masks} width='80' alt='happy and sad theater masks'></img> */}
 
+      </div>
+      
+      <span style={{fontSize: 'calc(12px + 2vmin)',margin: 20}}>Buscá una obra de teatro para ver hoy y ahora de manera fácil</span>
+      {/* <img src={masks} width='180' alt='happy and sad theater masks'></img>       */}
     </header>
   )
 }
