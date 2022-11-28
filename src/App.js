d99cab4162654b1bd9974a5ab162c4cc3e6e2de7
EzequielRomio <romio.ezequiel@gmail.com>
2022-11-28 12:30:51 -0300

    style updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react'
 import { getTheaters, getDay, getBillboard } from './actions';
-// import Theaters from './components/Theaters';
 import Plays from './components/Plays';
-import Title from './components/Title';
 import './App.css';
 
 function App() {
@@ -18,8 +16,6 @@ function App() {
 
   return (
     <div className="App">
-      <Title />
-      <hr style={{height: '26px', backgroundColor: 'burlywood', border: 'burlywood', boxShadow: '5px 5px 20px #000'}}></hr>
       {
         theaters && 
         theaters.length && 
@@ -27,7 +23,6 @@ function App() {
         billboard.length && 
         <Plays theaters={theaters} billboard={billboard} currentDay={currentDay} />  
       }
-      {/* {theaters && theaters.length && <Theaters theaters={theaters}/>} */}
     </div>
   );
 }
